6b79f4da3ed1bd54aa4f0d501f5501d2b0599188
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-11-06 12:09:36 -0800

    Layer

---
+++ Data/Layer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HD
+{
+  [Serializable]
+  public struct Layer
+  {
+    [SerializeField]
+    int layerIndex;
+
+    public Layer(
+      int layerIndex)
+    {
+      this.layerIndex = layerIndex;
+    }
+
+    public int mask
+    {
+      get
+      {
+        return 1 << layerIndex;
+      }
+    }
+
+    public static implicit operator int(
+      Layer layer)
+    {
+      return layer.layerIndex;
+    } 
+  }
+}
