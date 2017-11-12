21293f14898cbc22d113125795ca86f444cf5b79
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-11-12 11:11:50 -0800

    ..

---
+++ Data/Sort/RaycastHitSortByPointMag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HD
+{
+  public class RaycastHitSortByPointMag : IComparer<RaycastHit>
+  {
+    readonly Vector3 fromPosition;
+
+    public RaycastHitSortByPointMag(
+      Vector3 fromPosition)
+    {
+      this.fromPosition = fromPosition;
+    }
+
+    public int Compare(
+      RaycastHit x, 
+      RaycastHit y)
+    {
+      return (x.point - fromPosition).sqrMagnitude.CompareTo((y.point - fromPosition).sqrMagnitude);
+    }
+  }
+}
---
+++ MonoBehaviour/OnXSetActiveFalse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace HD
+{
+  public class OnXSetActiveFalse : MonoBehaviour
+  {
+    public enum EventType
+    {
+      Awake, Start
+    }
+    [SerializeField]
+    EventType eventType;
+
+    protected void Awake()
+    {
+      TryChange(EventType.Awake);
+    }
+
+    protected void Start()
+    {
+      TryChange(EventType.Start);
+    }
+
+    void TryChange(
+      EventType eventType)
+    {
+      if(this.eventType != eventType)
+      {
+        return;
+      }
+
+      Debug.Assert(enabled);
+
+      gameObject.SetActive(false);
+    }
+  }
+}
