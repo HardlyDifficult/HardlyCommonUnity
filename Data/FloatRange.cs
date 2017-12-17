d362ef8a028000b2b6a1d4fde54dd189d070e6c7
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-17 08:37:16 -0800

    Added float range.

---
+++ Data/FloatRange.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpaceTrade.Scripts
+{
+  /// <summary>
+  /// Select rng values within a given range.
+  /// </summary>
+  [Serializable]
+  public class FloatRange
+  {
+    [SerializeField]
+    float _minInclusive;
+
+    [SerializeField]
+    float _maxInclusive;
+
+    public float minInclusive
+    {
+      get
+      {
+        return _minInclusive;
+      }
+    }
+
+    public float maxInclusive
+    {
+      get
+      {
+        return _maxInclusive;
+      }
+    }
+
+    public float RandomValue()
+    {
+      return UnityEngine.Random.Range(minInclusive, maxInclusive);
+    }
+
+    public static implicit operator float(
+      FloatRange range)
+    {
+      return range.RandomValue();
+    }
+  }
+}
